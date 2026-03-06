ac79352274d5b229d49397aafc5306944cfd84df
BoozeSlinger <162624584+BoozeSlinger@users.noreply.github.com>
2026-03-05 19:04:55 -0800

    Update supabase.ts

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = 'https://mfhvdruyalxqssigaijs.supabase.co';
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1maHZkcnV5YWx4cXNzaWdhaWpzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjcyNzc3NzQsImV4cCI6MjA4Mjg1Mzc3NH0.W-ISj2LTEGHaXzW3iMXX8hT9Wec96HH8op2bYzum2p8';
+const supabaseUrl = 'https://zrncgoajwlisttvzvwdr.supabase.co';
+const supabaseAnonKey = 'sb_publishable_nmeO8YjdO-BHdfY_c0W_Yg_cnDi1Mkd';
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
