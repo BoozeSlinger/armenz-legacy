4ef66dc74063c0aab9cb2b54edcc509fb21741ef
Ryan Stevens <ryanhustlesie@gmail.com>
2026-03-11 18:40:16 -0700

    fix: correct morongo logo case for vercel deployment

--- src/components/ConfirmedSponsors.tsx
+++ src/components/ConfirmedSponsors.tsx
@@ -16,7 +16,7 @@ const confirmedSponsors: Sponsor[] = [
     name: "Morongo Band of Mission Indians",
     tier: "Triple Crown",
     url: "https://www.morongonation.org",
-    logo: "/images/morongologo.png",
+    logo: "/images/Morongologo.png",
   },
 ];
 
